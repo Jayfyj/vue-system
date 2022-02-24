3dd13a633778b557eb6df30ea5b06455d8b7cf3b
Jayfyj <jay123jie@163.com>
2022-02-24 14:58:56 +0800

    调整

--- src/views/allSelectTable/api.js
+++
@@ -1,9 +0,0 @@
-import {instance} from '@/utils/request';
-
-
-export const tableDataUrl = (body) =>instance.get('/server/table',{ params: body })
-
-export const tabjsonUrl = (body) =>instance.get('/server/tabjson',{ params: body })
-
-
-
