9c9147581fdbee95db7cb2f4d550d6e539ef7e98
Jayfyj <jay123jie@163.com>
2022-02-25 14:13:37 +0800

    继续调整页面

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,8 +2,5 @@
 import './menuUrl'
 //全选所有列表数据
 import './table'
-import './table/tabjson'
 
-import './user'
-import './remberScroll'
 
--- src/mock/table/index.js
+++ src/mock/table/index.js
@@ -100,6 +100,31 @@ const data=
     "total": 12
 };
 
+const data2=
+{
+    "list": [
+        {
+            "id": 11,
+            "name": "王五",
+            "money": 789,
+            "address": "湖南省长沙市",
+            "state": "失败",
+            "date": "2019-11-11",
+            "thumb": "https://lin-xin.gitee.io/images/post/parcel.png"
+        },
+        {
+            "id": 12,
+            "name": "赵六",
+            "money": 1011,
+            "address": "福建省厦门市鼓浪屿",
+            "state": "成功",
+            "date": "2019-10-20",
+            "thumb": "https://lin-xin.gitee.io/images/post/notice.png"
+        }
+    ],
+    "total": 12
+};
+
 Mock.setup({
     timeout: '1000' // 表示响应时间介于 200 和 600 毫秒之间，默认值是'10-100'。
 })
@@ -108,5 +133,10 @@ Mock.mock(RegExp("/server/table" + ".*"), 'get', (options)=>{
   // console.log(options)
   return Mock.mock({ data, code: '0000' })
 })
+
+Mock.mock(RegExp("/server/tabjson" + ".*"), 'get', (options)=>{
+    // console.log(options)
+    return Mock.mock({ data:data2, code: '0000' })
+  })
 
 export default Mock;
